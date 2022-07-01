77cba6fa5abf14b11eca68f4036fb51e9c403694
Flordeliza Mae Canlas <maemaeperlas@gmail.com>
2022-07-01 14:39:20 -0700

    commit tree 3d5a0e825fb1f14a65c65c409a80d1087888e1ee

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+let x = document.getElementById("main");
+x.remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.setAttribute("id","victory");
+
+const newContent = document.createTextNode("YOUR-NAME is the champion");
+
+newHeader.appendChild(newContent);
